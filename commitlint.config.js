e89580ca2583eabac4f8617e764f303f33ab559d
Camilo Muñoz <camilo.mh2014@gmail.com>
2026-06-19 18:50:30 -0400

    chore(setup): bootstrap project tooling and config

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/**
+ * Configuración de commitlint: exige mensajes de commit con el formato
+ * Conventional Commits (p. ej. `feat: ...`, `fix(scope): ...`).
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
